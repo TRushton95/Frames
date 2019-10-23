5a60cb9a1f2b982edb5713ac5f8c6757fb72a183
Tom Rushton <tom.rushton@hotmail.co.uk>
2019-10-23 23:31:44 +0100

    Create BaseElement class

---
+++ Frames/UI/Elements/BaseElement.cs
@@ -0,0 +1,56 @@
+﻿namespace Frames.UI.Elements
+{
+    /// <summary>
+    /// The base structure of a user interface element.
+    /// </summary>
+    public abstract class BaseElement
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the priority.
+        /// </summary>
+        public int Priority
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the element is visible.
+        /// </summary>
+        public bool Visible
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the element is interactive.
+        /// </summary>
+        public bool Interactive
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the element blocks the ui.
+        /// </summary>
+        public bool Blocker
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the element is hovered.
+        /// </summary>
+        public bool Hovered
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
