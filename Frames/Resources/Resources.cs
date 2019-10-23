f497abcbd16d2b4620893135b62a006cf4c68d42
Tom Rushton <tom.rushton@hotmail.co.uk>
2019-10-23 20:47:43 +0100

    Create singleton Resources class

---
+++ Frames/Resources/Resources.cs
@@ -0,0 +1,56 @@
+﻿namespace Frames.Resources
+{
+    #region Usings
+
+    using Microsoft.Xna.Framework.Graphics;
+
+    #endregion
+
+    public class Resources
+    {
+        #region Fields
+
+        private static Resources _instance;
+
+        private GraphicsDevice graphicsDevice;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the singleton instance.
+        /// </summary>
+        public static Resources Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Resources();
+                }
+
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the graphics device.
+        /// </summary>
+        public GraphicsDevice GraphicsDevice => graphicsDevice;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Initialise the resources.
+        /// </summary>
+        public void Initialise(GraphicsDevice graphicsDevice)
+        {
+            this.graphicsDevice = graphicsDevice;
+        }
+
+        #endregion
+    }
+}
