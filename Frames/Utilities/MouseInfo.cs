f902e11b10b18f87fce49c09962134dd9dc817af
Tom Rushton <tom.rushton@hotmail.co.uk>
2019-10-26 12:38:51 +0100

    Add MouseInfo helper class

---
+++ Frames/Utilities/MouseInfo.cs
@@ -0,0 +1,77 @@
+﻿namespace Frames.Utilities
+{
+    #region Usings
+
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Input;
+
+    #endregion
+
+    /// <summary>
+    /// A wrapper class for <see cref="MouseState"/> that provides easy access to the changing of mouse states.
+    /// </summary>
+    public static class MouseInfo
+    {
+        #region Fields
+
+        private static MouseState currentState, previousState;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Updates the mouse state.
+        /// </summary>
+        public static void Update()
+        {
+            previousState = currentState;
+            currentState = Mouse.GetState();
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the x position of the mouse.
+        /// </summary>
+
+        public static int X => currentState.X;
+
+        /// <summary>
+        /// Gets the y position of the mouse.
+        /// </summary>
+        public static int Y => currentState.Y;
+
+        /// <summary>
+        /// Gets the position of the mouse.
+        /// </summary>
+        public static Vector2 Position => currentState.Position.ToVector2();
+
+        /// <summary>
+        /// Gets a value indicating whether the left mouse button is currently down.
+        /// </summary>
+
+        public static bool LeftMouseDown => currentState.LeftButton == ButtonState.Pressed;
+
+        /// <summary>
+        /// Gets a value indicating whether the right mouse button is currently down.
+        /// </summary>
+        public static bool RightMouseDown => currentState.RightButton == ButtonState.Pressed;
+
+        /// <summary>
+        /// Checks if the left mouse button has just been clicked.
+        /// </summary>
+        public static bool LeftMouseClicked => currentState.LeftButton == ButtonState.Pressed &&
+                                                previousState.LeftButton == ButtonState.Released;
+
+        /// <summary>
+        /// Checks if the right mouse button has just been clicked.
+        /// </summary>
+        public static bool RightMouseClicked => currentState.LeftButton == ButtonState.Pressed &&
+                                                previousState.LeftButton == ButtonState.Released;
+
+        #endregion
+    }
+}
