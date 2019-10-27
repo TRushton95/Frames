d401d274adaa5b9f640e1574bcd45fd963497f9e
Tom Rushton <tom.rushton@hotmail.co.uk>
2019-10-27 22:41:47 +0000

    Add comments

--- Frames/UserInterface/Elements/Icon.cs
+++ Frames/UserInterface/Elements/Icon.cs
@@ -11,6 +11,9 @@
 
     #endregion
 
+    /// <summary>
+    /// The icon element.
+    /// </summary>
     public class Icon : BaseElement
     {
         #region Fields
@@ -21,6 +24,9 @@
 
         #region Constructors
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Icon"/> class.
+        /// </summary>
         public Icon(string name, Texture2D texture, IPositionProfile positionProfile)
             : base (name, texture.Width, texture.Height, positionProfile)
         {
@@ -31,6 +37,9 @@
 
         #region Properties
 
+        /// <summary>
+        /// Gets the texture.
+        /// </summary>
         public Texture2D Texture
         {
             get;
@@ -40,21 +49,33 @@
 
         #region Methods
 
+        /// <summary>
+        /// Draws the element.
+        /// <param name="spriteBatch"></param>
         public override void Draw(SpriteBatch spriteBatch)
         {
             this.imageGraphics.Draw(spriteBatch);
         }
 
+        /// <summary>
+        /// Gets the size.
+        /// </summary>
         public override Size GetSize()
         {
             return new Size(this.Width, this.Height);
         }
 
+        /// <summary>
+        /// Provides the initialisation behaviour specific to the implementing component.
+        /// </summary>
         protected override void InternalInitialise(Rectangle parent)
         {
             this.BuildComponents(parent);
         }
 
+        /// <summary>
+        /// Builds the components.
+        /// </summary>
         private void BuildComponents(Rectangle parent)
         {
             this.imageGraphics = new ImageGraphics(this.Texture, PositionFactory.CenteredRelative());
@@ -65,14 +86,23 @@
 
         #region Internal Interaction Handlers 
 
+        /// <summary>
+        /// The implementation details for the <see cref="Hover"/> method.
+        /// </summary>
         protected override void HoverDetail()
         {
         }
 
+        /// <summary>
+        /// The implementation details for the <see cref="HoverLeave"/> method.
+        /// </summary>
         protected override void HoverLeaveDetail()
         {
         }
 
+        /// <summary>
+        /// The implementation details for the <see cref="LeftClick"/> method.
+        /// </summary>
         protected override void LeftClickDetail()
         {
         }
