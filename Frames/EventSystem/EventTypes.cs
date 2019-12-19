e1eb866062b0f5f0a91dac92e5399596be09a2f4
Tom Rushton <tom.rushton@hotmail.co.uk>
2019-12-19 23:47:09 +0000

    Add sender name to event firing

--- Frames/EventSystem/EventTypes.cs
+++ Frames/EventSystem/EventTypes.cs
@@ -4,11 +4,11 @@
     {
         public static class Frames
         {
-            public static EventType ElementHover = EventType.GetEventType("FRAMES_ELEMENT-HOVER");
-            public static EventType ElementHoverLeave = EventType.GetEventType("FRAMES_ELEMENT-HOVER-LEAVE");
-            public static EventType ElementClick = EventType.GetEventType("FRAMES_ELEMENT-CLICK");
-            public static EventType ElementMouseWheelScrollUp = EventType.GetEventType("FRAMES_ELEMENT_MOUSE_WHEEL_SCROLL_UP");
-            public static EventType ElementMouseWheelScrollDown = EventType.GetEventType("FRAMES_ELEMENT_MOUSE_WHEEL_SCROLL_DOWN");
+            public static string ElementHover = "FRAMES_ELEMENT-HOVER";
+            public static string ElementHoverLeave = "FRAMES_ELEMENT-HOVER-LEAVE";
+            public static string ElementClick = "FRAMES_ELEMENT-CLICK";
+            public static string ElementMouseWheelScrollUp = "FRAMES_ELEMENT_MOUSE_WHEEL_SCROLL_UP";
+            public static string ElementMouseWheelScrollDown = "FRAMES_ELEMENT_MOUSE_WHEEL_SCROLL_DOWN";
         }
     }
 }
