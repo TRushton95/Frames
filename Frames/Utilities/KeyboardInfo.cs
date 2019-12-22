ed115e3e69707b1c17f6469904059964b6511b5a
Tom Rushton <tom.rushton@hotmail.co.uk>
2019-12-22 12:46:04 +0000

    Create KeyboardInfo class

---
+++ Frames/Utilities/KeyboardInfo.cs
@@ -0,0 +1,46 @@
+﻿namespace Frames.Utilities
+{
+    #region Usings
+
+    using Microsoft.Xna.Framework.Input;
+
+    #endregion
+
+    public static class KeyboardInfo
+    {
+        #region Fields
+
+        private static KeyboardState currentState, previousState;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Updates the keyboard state.
+        /// </summary>
+        public static void Update()
+        {
+            previousState = currentState;
+            currentState = Keyboard.GetState();
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the key is down.
+        /// </summary>
+        public static bool IsKeyDown(Keys key)
+        {
+            return currentState.IsKeyDown(key);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the key has just been pressed.
+        /// </summary>
+        public static bool IsKeyPressed(Keys key)
+        {
+            return currentState.IsKeyDown(key) && !previousState.IsKeyDown(key);
+        }
+
+        #endregion
+    }
+}
